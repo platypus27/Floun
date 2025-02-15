cd66da9886d654cea18d5dccb1c929eef92481db
Ao Yu <84365804+platypus27@users.noreply.github.com>
2025-02-15 21:25:56 +0800

    built react app and css works on extension

--- floun/webpack.config.js
+++ floun/webpack.config.js
@@ -25,7 +25,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './src/index.html', // Path to your HTML template
+      template: './public/index.html', // Path to your HTML template
       filename: 'index.html' // Output HTML file name
     })
   ]
