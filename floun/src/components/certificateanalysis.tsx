b86584f3b9f24b50cfc2fbc5dd4843834a302b2e
jjjjjaayyyyyyyyyyy <jjay05lim@gmail.com>
2025-03-02 01:15:45 +0800

    updated certificate analysis

--- floun/src/components/certificateanalysis.tsx
+++ floun/src/components/certificateanalysis.tsx
@@ -2,24 +2,14 @@
 // find the cryptographic algorithm used in the certificate and determine if it is quantum resistant
 // semi easy i guess shouldnt be hard at all
 
-const QUANTUM_SAFE_SIGNATURES = [
-  "dilithium",
-  "falcon",
-  "sphincs",
-  "crystals-dilithium",
-  "crystals-kyber",
-];
+import { CERTIFICATE_SIGNATURES } from './cryptographyAnalysisEngine';
 
-export const analyzeCertificate = (certificate: any): Record<string, string> | null => {
+export const analyzeCertificate = (certificate: any): string | null => {
   const certificate_algo = certificate["result"]["cert_alg"];
-  const analysisResult: Record<string, string> = {};
 
-  if (QUANTUM_SAFE_SIGNATURES.includes(certificate_algo)) {
-    analysisResult["Signature Algorithm"] = "Certificate is Quantum-Safe";
+  if (CERTIFICATE_SIGNATURES['safe'].includes(certificate_algo)) {
+    return `Found ${certificate_algo} [Safe] in certificate`
   } else {
-    analysisResult["Signature Algorithm"] =
-      "Certificate Not Quantum-Safe, Hash Result: " + certificate_algo;
+    return `Found ${certificate_algo} [Vulnerable] in certificate`
   }
-
-  return analysisResult;
 };
